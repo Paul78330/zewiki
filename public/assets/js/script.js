53e2cd3892858a9aef6859a5d77cbe349211861e
stephane <chaaos_stephane@hotmail.com>
2024-05-26 23:11:58 +0200

    test responsive

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,16 +1,3 @@
-/*
-addEventListener("DOMContentLoaded", start);
-function start(){
-    var toast = document.querySelector('.toast');
-        setTimeout(function(){
-        toast.classList.remove('show');
-        if (toast.dataset.redirect !== 'undefined' )
-        {
-            window.location.replace(toast.dataset.redirect);
-        }
-    },10000);
-
-}*/
 function start() {
     // Fonction pour décrémenter la valeur d'un élément par son ID
     function decrementValueById(elementId) {
@@ -52,3 +39,48 @@ function start() {
 
 // Démarre la fonction start lorsque le DOM est chargé
 document.addEventListener("DOMContentLoaded", start);
+
+
+
+// -----  Gestion de  l'affichage du body pour qu'il soit toujours juste en dessous de la navbar dont la hauteur varie lorsqu'on est en mobile et on clique le menu hamburger ----- 
+// Adapte le padding-top au début (évite d'avoir a indiquer une valeur initiale )
+adjustPaddingTop();
+// fonction qui adapte la taille du padding top du body en fonction de la hauteur de la navbar ( lorsqu'on clique sur le menu hamburger la taille augmente)
+function adjustPaddingTop() {
+    const navbarHeight = document.querySelector('.navbar').offsetHeight;
+    document.body.style.paddingTop = navbarHeight + 'px';
+}
+
+
+
+// Creation d'un listener qui ecoute le clic sur le menu hamburger
+document.querySelector('.navbar-toggler').addEventListener('click', function () {
+setTimeout(adjustPaddingTop, 300); // Délai pour laisser le temps à l'animation de se terminer
+});
+
+
+
+// -----  Gestion du changement de répartition de taille entre les deux panneaux ( gauche et droite) en déplacant le séparateur
+
+// on récupere les trois éléments ( deux panneaux et séparateur)
+const sparator = document.getElementById('separator');
+const colonne1 = document.getElementById('leftpanel');
+const colonne2 = document.getElementById('rightpanel');
+
+// on ajoute un listener sur le clique au niveau du séparateur
+sparator.addEventListener('mousedown', function(e) {
+    e.preventDefault();
+    document.addEventListener('mousemove', startResize);
+    document.addEventListener('mouseup', stopResize);
+});
+
+function startResize(e) {
+    const newWidth = e.clientX / window.innerWidth * 100;
+    colonne1.style.width = `${newWidth}%`;
+    colonne2.style.width = `${100 - newWidth}%`;
+}
+
+function stopResize() {
+    document.removeEventListener('mousemove', startResize);
+    document.removeEventListener('mouseup', stopResize);
+}
