1c199c0c32b0d4e74aeba1829349bf0217b9d9da
stephane_greta <chaaos_stephane@hotmail.com>
2024-05-30 12:42:49 +0200

    change db adresse

--- app/models/DatabaseManager.php
+++ app/models/DatabaseManager.php
@@ -12,12 +12,11 @@ class DatabaseManager{
 
 # -------------------- Constructor --------------------
     public function __construct() {
-        $this->pdo = new PDO("mysql:host=127.0.0.1;dbname=zewiki;charset=utf8;port=3306", "zewikiphpuser","zepdfife85*-zd");
+        $this->pdo = new PDO("mysql:host=192.168.0.50;dbname=zewiki;charset=utf8;port=3306", "zewikiphpuser","zepdfife85*-zd");
         // change en 192.168.0.50 avant git pull
     }
 
-
-# -------------------- Methods --------------------
+## -------------------- Methods --------------------
 
 
 # -------------------------------------------------------------------------------------------------------------------------------------------------------------------------
